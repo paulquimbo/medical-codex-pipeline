0bd71c1f49c0ddc3ef43d5a28ff298bc7bb8cc17
= <=>
2025-09-04 23:20:36 -0400

    updating .gitnore  and loinc

---
+++ scripts/loinc_processor.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+# Loading Data Set and have it as a variable
+loinc = pd.read_csv('input/loinc/Loinc.csv')
+
+# Dispays rows and column information of the dataframe
+loinc.info()
+
+# Displays the first 5 rows of the dataframe
+print(loinc.head())
+
+# Column Exploration and checking potental columns of interest
+## Displays the column information of the dataframe
+loinc.LOINC_NUM
+loinc.LONG_COMMON_NAME
+loinc.SHORTNAME
+loinc.DisplayName
+loinc.DefinitionDescription
+
+### Selecting columns of interest and adding to a new dataframe
+shortloinc = loinc[['LOINC_NUM', 'LONG_COMMON_NAME']]
+
+# adding a new column to the new dataframe with a default value
+shortloinc ['last_updated'] = '09-04-2025'
+
+# renaming column names from shortlist
+shortloinc = shortloinc.rename(columns={
+    'LOINC_NUM': 'Code',
+    'LONG_COMMON_NAME': 'Description'
+})
+
+shortloinc
+
+shortloinc.to_csv('output/csv/loinc_short.csv', index=False)
